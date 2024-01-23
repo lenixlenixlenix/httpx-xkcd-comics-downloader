60ee5ce9b7d4b1f95b43aa99ee5a5d29334cfce0
lenixlenixlenix <dileron664@gmail.com>
2024-01-23 19:48:11 +0300

    async version

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import httpx
 import os
+import asyncio
 from bs4 import BeautifulSoup
 
 def download_file(url, path) -> None:
@@ -18,16 +19,31 @@ else:
     os.mkdir("downloaded")
 url = "https://xkcd.com/"
 
-last_index_comix = 2883
+last_index_comix = 10
 
-for index in range(1, last_index_comix):
-    r = httpx.get(os.path.join(url, str(index) + "/"))
+async def inspect_file(index):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(os.path.join(url, str(index) + "/"))
+        soup = BeautifulSoup(r, "html5lib")
+        div = soup.find('div', {'id':'comic'})
 
-    soup = BeautifulSoup(r, "html5lib")
-    div = soup.find('div', {'id':'comic'})
+        img_src = div.find('img').attrs['src']
+        file_name = "xkcd#" + str(index) +".png"
+        download_file("https:" + img_src, os.path.join(folder_path, file_name))
+        
 
-    img_src = div.find('img').attrs['src']
-    file_name = "xkcd#" + str(index) +".png"
-    download_file("https:" + img_src, os.path.join(folder_path, file_name))
+    
+async def main():
+    tasks = []
+
+    for index in range(1, last_index_comix):
+        tasks.append(inspect_file(index))
+    results = await asyncio.gather(*tasks)
+
+asyncio.run(main())
+
+        
+
+    
 
 
