3a8728a6647f1608a2c233f8703c7507874067bb
lenixlenixlenix <dileron664@gmail.com>
2024-01-23 19:21:55 +0300

    sync version

---
+++ main.py
@@ -0,0 +1,33 @@
+import httpx
+import os
+from bs4 import BeautifulSoup
+
+def download_file(url, path) -> None:
+    with httpx.stream('GET', url) as resp:
+        with open(path, "wb") as out_file:
+            for chunk in resp.iter_bytes():
+                out_file.write(chunk)
+
+folder_path = os.path.join(os.path.curdir, "downloaded")
+
+print(folder_path)
+
+if (os.path.isdir(folder_path)):
+    pass
+else:
+    os.mkdir("downloaded")
+url = "https://xkcd.com/"
+
+last_index_comix = 2883
+
+for index in range(1, last_index_comix):
+    r = httpx.get(os.path.join(url, str(index) + "/"))
+
+    soup = BeautifulSoup(r, "html5lib")
+    div = soup.find('div', {'id':'comic'})
+
+    img_src = div.find('img').attrs['src']
+    file_name = "xkcd#" + str(index) +".png"
+    download_file("https:" + img_src, os.path.join(folder_path, file_name))
+
+
